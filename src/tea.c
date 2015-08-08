318ea75863b6a7dfae6fde56242045528ad75c7a
Michael Zhu <boot2linux@gmail.com>
2015-08-08 08:37:51 +0800

    fix no alarm if no idle serial port

--- src/tea.c
+++ src/tea.c
@@ -6,12 +6,10 @@
 #include "serial.h"
 #include "xfer.h"
 
-
 int main(int argc, char *argv[])
 {
     int number;
     int fd;
-    char buf[1024];
 
     number = scan_serial();
     if ( number == 0 )
@@ -21,13 +19,14 @@ int main(int argc, char *argv[])
     }
 
     fd = open_one_idle_serial();
-    if ( !fd )
+    if ( fd < 0 )
     {
-        fprintf(stderr, "No idel serial port!\n");
+        fprintf(stderr, "No idle serial port!\n");
         exit(1);
     }
 
     xfer_init();
+
     new_emulator(fd, 0, 1);
 
     xfer_loop();
