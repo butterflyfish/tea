69a774964e0dc07dd6c0f6d82b55cf1d7b13839d
Michael Zhu <boot2linux@gmail.com>
2015-10-08 21:30:45 +0800

    bump version to 2015.10.8

--- src/tea.c
+++ src/tea.c
@@ -37,7 +37,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "serial.h"
 #include "terminal.h"
 
-static char *ver = "2005.8.16";
+static char *ver = "2005.10.8";
 
 static
 void usage()
