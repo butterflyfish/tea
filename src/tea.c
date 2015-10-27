74af14ee535913275d656d4427dae3ec4249f26d
Michael Zhu <boot2linux@gmail.com>
2015-10-27 12:44:44 +0800

    start next development

--- src/tea.c
+++ src/tea.c
@@ -37,7 +37,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "serial.h"
 #include "terminal.h"
 
-static char *ver = "2005.10.8";
+static char *ver = "2005.x.y";
 
 static
 void usage()
